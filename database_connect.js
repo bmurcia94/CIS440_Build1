0640bbeea68ae0cbafd097df01fb0fecd89cf57a
bryant.murcia <143425870+bmurcia94@users.noreply.github.com>
2023-09-21 21:05:33 -0700

    Create database_connect.js

---
+++ database_connect.js
@@ -0,0 +1,15 @@
+var mysql = require("mysql")
+
+var con = mysql.createConnection({
+    host: "107.180.1.16",
+    port: "3306"
+    user: "fall2023team1",
+    password: "fall2023team1",
+    database: "fall2023team1"
+});
+//con.connect();
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
