e9f5be1c27758d97d5a897740286c079e5825b66
bryant.murcia <143425870+bmurcia94@users.noreply.github.com>
2023-09-21 21:10:27 -0700

    Update database_connect.js

--- database_connect.js
+++ database_connect.js
@@ -2,7 +2,7 @@ var mysql = require("mysql")
 
 var con = mysql.createConnection({
     host: "107.180.1.16",
-    port: "3306"
+    port: "3306",
     user: "fall2023team1",
     password: "fall2023team1",
     database: "fall2023team1"
