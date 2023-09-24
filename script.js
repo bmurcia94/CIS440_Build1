ab6f9e6b6030985b5499a48493b91b2925ee0f0a
bryant.murcia <143425870+bmurcia94@users.noreply.github.com>
2023-09-24 01:29:51 -0700

    Update script.js
    Added login function

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function newPage(page) {
 
 }
 
-document.getElementById("defaultOpen").click();
+//document.getElementById("defaultOpen").click();
 
 
 function changePage(pageID) {
@@ -22,3 +22,28 @@ function changePage(pageID) {
     var selectedPage = document.getElementById(pageID);
     selectedPage.style.display = 'block';
 }
+
+//function that allows users to login
+function userLogin() {
+    var userName = document.getElementById('username').value; //grab values from username textbox
+    var userPass = document.getElementById('password').value; //grab values from password textbox
+    var user; //create variable that will hold table information
+
+
+        $.get("?tableName=User", function (userTable) {     //JQuery GET method to request an array of information from database
+            //console.log(userTable);
+            user = JSON.parse(userTable);       //converts array data to JSON
+
+            var checkUser = user.find(function (user) {         // variable that will use find method to search array for specific values
+                return user.userName === userName && user.userpassword === userPass;        //checks whether username and its corresponding password are the same 
+            });
+
+            if (checkUser) {
+                alert("Login successful!"); //if both are correlated, success message
+            }
+            else {
+                alert("Login Failed. Try again"); //if both are not correlated, failed message
+            }      
+            }); //end if-else
+
+} //end function
