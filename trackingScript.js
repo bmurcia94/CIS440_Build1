22be5574c572b2faae17d8a141b809fc4c3fe30f
nickandrews010 <143490239+nickandrews010@users.noreply.github.com>
2023-09-27 23:26:07 -0700

    Update trackingScript.js

--- trackingScript.js
+++ trackingScript.js
@@ -33,46 +33,56 @@ function updateChart() {
     progressChart.destroy();
   }
 
+  let allWeeks = [];
+  for (let exercise in ormData) {
+    allWeeks = allWeeks.concat(Object.keys(ormData[exercise]));
+  }
+
+  if (allWeeks.length === 0) {
+    allWeeks.push("1");
+  }
+
+  const uniqueSortedWeeks = [...new Set(allWeeks)].map(Number).sort((a, b) => a - b);
+  const extendedWeeks = Array.from({ length: uniqueSortedWeeks[uniqueSortedWeeks.length - 1] }, (_, i) => i + 1);
+
+  const datasets = [];
+  const colors = [
+    'rgba(75, 192, 192, 1)', 
+    'rgba(255, 99, 132, 1)', 
+    'rgba(255, 206, 86, 1)', 
+    'rgba(153, 102, 255, 1)', 
+    'rgba(54, 162, 235, 1)', 
+    'rgba(255, 159, 64, 1)', 
+    'rgba(201, 203, 207, 1)'
+  ];
+
+  const exerciseLabels = {
+    squat: 'Squat',
+    deadlift: 'Deadlift',
+    benchpress: 'Bench Press',
+    pullup: 'Pull-up',
+    bicepcurl: 'Bicep Curl',
+    triceppushdown: 'Triceps Pushdown',
+    shoulderpress: 'Shoulder Press'
+  };
+
+  let colorIndex = 0;
+  for (let exercise in ormData) {
+    datasets.push({
+      label: exerciseLabels[exercise],
+      data: extendedWeeks.map(week => (week in ormData[exercise]) ? ormData[exercise][week] : null),
+      borderColor: colors[colorIndex % colors.length],
+      fill: false,
+      spanGaps: true
+    });
+    colorIndex++;
+  }
+
   progressChart = new Chart(ctx, {
     type: 'line',
     data: {
-      labels: Object.keys(ormData.squat).map(Number).sort((a, b) => a - b),
-      datasets: [{
-        label: 'Squat',
-        data: Object.values(ormData.squat),
-        borderColor: 'rgba(75, 192, 192, 1)',
-        fill: false
-      }, {
-        label: 'Deadlift',
-        data: Object.values(ormData.deadlift),
-        borderColor: 'rgba(255, 99, 132, 1)',
-        fill: false
-      }, {
-        label: 'Bench Press',
-        data: Object.values(ormData.benchpress),
-        borderColor: 'rgba(255, 206, 86, 1)',
-        fill: false
-      }, {
-        label: 'Pull Up',
-        data: Object.values(ormData.pullup),
-        borderColor: 'rgba(153, 102, 255, 1)',
-        fill: false
-      }, {
-        label: 'Bicep Curl',
-        data: Object.values(ormData.bicepcurl),
-        borderColor: 'rgba(54, 162, 235, 1)',
-        fill: false
-      }, {
-        label: 'Tricep Pushdown',
-        data: Object.values(ormData.triceppushdown),
-        borderColor: 'rgba(255, 159, 64, 1)',
-        fill: false
-      }, {
-        label: 'Shoulder Press',
-        data: Object.values(ormData.shoulderpress),
-        borderColor: 'rgba(201, 203, 207, 1)',
-        fill: false
-      }]
+      labels: extendedWeeks,
+      datasets: datasets
     },
     options: {
       responsive: true,
@@ -87,6 +97,7 @@ function updateChart() {
     }
   });
 }
+
 function clearChartData() {
   ormData.squat = [];
   ormData.deadlift = [];
@@ -96,9 +107,9 @@ function clearChartData() {
   ormData.triceppushdown = [];
   ormData.shoulderpress = [];
 
-
   updateChart();
 }
+
 const clearDataButton = document.getElementById('clearDataButton');
 clearDataButton.addEventListener('click', clearChartData);
 
